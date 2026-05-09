0bd35186859cc91717e92ca60585dd837f6d72e4
Abizzar <13523155@std.stei.itb.ac.id>
2026-05-09 21:13:20 +0700

    feat: model laporan operasional dengan workflow dan chatter (FR-01)

---
+++ custom_addons/plumeria_cafe/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Plumeria Cafe & Creative Space',
+    'summary': 'Sistem Informasi Operasional Plumeria Cafe - IF3141 K03-G05',
+    'version': '17.0.1.0',
+    'category': 'Custom',
+    'author': 'Kelompok 05 Kelas K03',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'data/sequence_data.xml',
+        'views/laporan_views.xml',
+        'views/transaksi_views.xml',
+        'views/produk_promo_views.xml',
+        'views/rekap_views.xml',
+        'views/dashboard_views.xml',
+        'views/menus.xml',
+        'data/demo_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/plumeria_cafe/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import laporan_operasional
+from . import transaksi_pos
+from . import produk_promo
+from . import rekap_keuangan
